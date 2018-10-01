27de180a308ae96bab9f5c366cf95f5fcdc82adf
Andrey Rogachevich <andrey.rogachevich@azati.com>
2018-10-02 00:09:13 +0300

    moduleRef global search

--- src/auth.guard.ts
+++ src/auth.guard.ts
@@ -1,10 +1,11 @@
 import { Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { JwtService } from '@nestjs/jwt';
+import { ModuleRef } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {
+  constructor(private readonly moduleRef: ModuleRef) {
 
   }
 
@@ -15,7 +16,7 @@ export class AuthGuard implements CanActivate {
 
   validateRequest(context, request): boolean {
     try {
-      this.jwtService.verify(request.headers['x-secret-token']);
+      this.moduleRef.get(JwtService, { strict: false }).verify(request.headers['x-secret-token']);
     } catch ({message}) {
       throw new HttpException(message, HttpStatus.I_AM_A_TEAPOT);
     }
